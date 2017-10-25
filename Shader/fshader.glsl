53b478e7c1c43e0c5ad8822f44945dfbd6215ef7
Morganeon <morganfouque@hotmail.fr>
2017-10-26 00:03:40 +0200

    cleanup

--- Shader/fshader.glsl
+++ Shader/fshader.glsl
@@ -1,12 +1,9 @@
 #version 330
 
-
 in vec2 v_texcoord;
-
 
 void main()
 {
     gl_FragColor = vec4(1.,0.2,0.,1);
-
 }
 
--- Shader/vshader.glsl
+++ Shader/vshader.glsl
@@ -3,9 +3,7 @@
 uniform mat4 mvp_matrix;
 in vec4 a_position;
 
-//! [0]
 void main()
 {
     gl_Position = mvp_matrix * a_position;
 }
-//! [0]
