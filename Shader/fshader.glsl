f4da055589bc9c59b42720dc51dcb4b150777b07
Anthony Brunel <anthony.brunel030@gmail.com>
2017-10-29 01:52:22 +0200

    Remove duplicated directory shader (transferred to resources)

--- Shader/fshader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec2 v_texcoord;
-
-void main()
-{
-    gl_FragColor = vec4(1.,0.2,0.,1);
-}
-
--- Shader/vshader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-uniform mat4 mvp_matrix;
-in vec4 a_position;
-
-void main()
-{
-    gl_Position = mvp_matrix * a_position;
-}
