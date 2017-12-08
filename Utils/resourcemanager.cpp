be7ac44ef8972a5148310248c89b024a75f8f2ea
Morgan-PC\Morgan <morganfouque@hotmail.fr>
2017-12-09 00:33:42 +0100

    ResourceManager V1

--- Utils/resourcemanager.cpp
+++ Utils/resourcemanager.cpp
@@ -1,6 +1,63 @@
 #include "Utils/resourcemanager.h"
 
-resourcemanager::resourcemanager()
+ResourceManager::ResourceManager()
 {
 
+}
+
+int ResourceManager::storeMesh(QString name, Mesh *meshPointer)
+{
+meshBatch.push_back(meshPointer);
+meshMap[meshBatch.size()] = name;
+return meshBatch.size();
+}
+
+int ResourceManager::storeShape(QString name, btCollisionShape *shapePointer)
+{
+    shapeBatch.push_back(shapePointer);
+    shapeMap[shapeBatch.size()] = name;
+    return shapeBatch.size();
+}
+
+Mesh *ResourceManager::retrieveMesh(int i)
+{
+    if (i>=meshBatch.size()) return nullptr;
+    return meshBatch.at(i);
+}
+
+btCollisionShape *ResourceManager::retrieveShape(int i)
+{
+    if (i>=shapeBatch.size()) return nullptr;
+    return shapeBatch.at(i);
+}
+
+
+Mesh *ResourceManager::retrieveMesh(QString name)
+{
+    int i = -1;
+    try
+    {
+        i= meshMap.at(name);
+    }
+    catch (std::exception e)
+    {
+        qDebug() << "WTF: " << e;
+    }
+    if (i==-1) return nullptr;
+    return meshBatch.at(i);
+}
+
+btCollisionShape *ResourceManager::retrieveShape(QString name)
+{
+    int i = -1;
+    try
+    {
+        i= shapeMap.at(name);
+    }
+    catch (std::exception e)
+    {
+        qDebug() << "WTF: " << e;
+    }
+    if (i==-1) return nullptr;
+    return shapeBatch.at(i);
 }
--- Utils/resourcemanager.h
+++ Utils/resourcemanager.h
@@ -2,15 +2,26 @@
 #define RESOURCEMANAGER_H
 
 #include<vector>
+#include<QString>
+#include<map>
+#include "external\bullet3-2.87\src\BulletCollision\btCollisionShape.h"
 
-
-class resourcemanager
+class ResourceManager
 {
 public:
-    resourcemanager();
+    ResourceManager();
+    ~ResourceManager();
+    int storeMesh(QString name, Mesh * meshPointer);
+    int storeShape(QString name, btCollisionShape * shapePointer);
+    Mesh * retrieveMesh(int);
+    btCollisionShape * retrieveShape(int);
+    Mesh * retrieveMesh(QString);
+    btCollisionShape * retrieveShape(QString);
 private:
-    std::vector<Mesh>meshBatch;
-    std::vector<Mesh>shapeBatch;
+    std::vector<Mesh *>meshBatch;
+    std::vector<btCollisionShape*>shapeBatch;
+    std::map<QString,int> meshMap;
+    std::map<QString,int> shapeMap;
 };
 
-#endif // UTILSESOURCEMANAGER_H
+#endif // RESOURCEMANAGER_H
