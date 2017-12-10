78ff9a8c5612871661a1c0bf11118cff8eac8fe6
Morgan-PC\Morgan <morganfouque@hotmail.fr>
2017-12-11 00:56:12 +0100

    Ship structure first version

--- Component/shipcomponent.cpp
+++ Component/shipcomponent.cpp
@@ -1,6 +1,81 @@
 #include "Component/shipcomponent.h"
 
-ShipComponent::ShipComponent()
+ShipComponent::ShipComponent(GameObject *gameObject, Scene *_scene):
+    Component(gameObject)
 {
+    scene = _scene;
+}
 
+void ShipComponent::update()
+{
+    /// Algorithm:
+    /// Progress along the circuit curve following acceleration.
+    /// Calculate vertical drag from circuit curve
+    /// Check for collision from circuit circuit limit
+    /// Bounce off and lose speed if in collision with circuit limit.
+    /// Check for circuit height changes, change ship accordingly
+
+    // Calculate new position on fragment
+    ProgressionOnFragment += FragmentSize/currentAcceleration;
+
+    if (ProgressionOnFragment >= 1)
+    {
+        ProgressionOnFragment-=1;
+        FragmentCount++;
+    }
+
+    // Calculate vertical drag
+
+    float VerticalDrag = 0.1; // Given by the circuit object, ranging from -1 to 1;
+
+    VerticalDistance+=VerticalDrag;
+    if (VerticalDistance > 1)
+    {
+        VerticalDistance = 1-(1-VerticalDistance);
+        currentAcceleration =MinAcceleration;
+    }
+    else if (VerticalDistance < 1)
+    {
+        VerticalDistance = -1+(-1-VerticalDistance);
+        currentAcceleration =MinAcceleration;
+    }
+
+    // Calculate height delta
+
+    float HeightDelta = 0.1; // Given by the circuite object, ranging from -1 to 1;
+
+    HorizontalDistance += HeightDelta;
+
+    if (HorizontalDistance > MaxHeight)
+    {
+        VerticalDistance = MaxHeight;
+    }
+    else if (HorizontalDistance < MinHeight)
+    {
+        VerticalDistance = MinHeight;
+    }
+}
+
+void ShipComponent::accelerate()
+{
+    currentAcceleration++;
+    if (currentAcceleration > MaxAcceleration) currentAcceleration=MaxAcceleration;
+}
+
+void ShipComponent::decelerate()
+{
+    currentAcceleration--;
+    if (currentAcceleration < MinAcceleration) currentAcceleration=MinAcceleration;
+}
+
+void ShipComponent::turnLeft()
+{
+    VerticalDistance -=0.05;
+    if (VerticalDistance <-1) VerticalDistance == -1;
+}
+
+void ShipComponent::turnRight()
+{
+    VerticalDistance +=0.05;
+    if (VerticalDistance >1) VerticalDistance == 1;
 }
--- Component/shipcomponent.h
+++ Component/shipcomponent.h
@@ -1,11 +1,38 @@
 #ifndef SHIPCOMPONENT_H
 #define SHIPCOMPONENT_H
 
+#include "external\bullet3-2.87\src\btBulletDynamicsCommon.h"
+#include "Game/scene.h"
+#include "QElapsedTimer"
 
-class ShipComponent
+class Component;
+class Scene;
+
+class ShipComponent : public Component
 {
 public:
-    ShipComponent();
+    ShipComponent(GameObject *gameObject, Scene * _scene);
+    virtual void update();
+    void accelerate();
+    void decelerate();
+    void turnLeft();
+    void turnRight();
+
+private:
+    Scene * scene; // To simulate world physic and raycasts
+
+    float FragmentCount; // Circuit fragment we're on
+    float ProgressionOnFragment;// % of fragment cleared
+
+    float currentAcceleration; // Speed at which the ship travels
+    float VerticalDistance; // Distance from center of the track to walls
+    float HorizontalDistance; // Distance from minimum to maximum flight altitude
+
+    const float MaxAcceleration = 40;
+    const float MinAcceleration = 10;
+    const float MaxHeight = 30;
+    const float MinHeight = 5;
+    const float FragmentSize = 1000;
 };
 
 #endif // SHIPCOMPONENT_H
